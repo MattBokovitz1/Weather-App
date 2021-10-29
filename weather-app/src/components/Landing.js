c1482d9fc68b1a590215c9755dc2a1cd25814c89
Matthew Bokovitz <MattBokovitz@Matthews-MBP-2.fios-router.home>
2021-10-29 11:02:35 -0400

    added search functionality and card to hold information about city's weather

--- weather-app/src/components/Landing.js
+++ weather-app/src/components/Landing.js
@@ -2,20 +2,28 @@ import React from "react";
 import styled from "styled-components";
 import sunny from "../media/sunnyimage.png";
 
-const PageWrapper = styled.div`
+const LandingWrapper = styled.div`
   display: flex;
+  font-family: "Roboto", sans-serif;
+  color: navy;
   justify-content: center;
   align-items: center;
-  margin-left: -40px;
+  height: 5%;
 `;
 
-export default function Landing() {
+const Sunny = styled.img`
+  width: 60%;
+`;
+
+function Landing() {
   return (
-    <PageWrapper>
+    <LandingWrapper>
       <h1>The Weather of the World </h1>
       <div>
-        <img src={sunny} alt="weather icon" />
+        <Sunny src={sunny} alt="weather icon" />
       </div>
-    </PageWrapper>
+    </LandingWrapper>
   );
 }
+
+export default Landing;
---
+++ weather-app/src/components/WeatherTable.js
@@ -0,0 +1,55 @@
+import React from "react";
+import styled from "styled-components";
+
+const WeatherCard = styled.div`
+  width: 40%;
+  margin: 3% auto;
+  padding: 3%;
+  background: darkblue;
+`;
+const SearchInstructions = styled.h3`
+  margin: auto;
+  padding: 5%;
+`;
+const DescriptionIcon = styled.img`
+  width: 30%;
+`;
+const WeatherInfo = styled.p`
+  color: #ffffff;
+`;
+const CityName = styled.h2`
+  color: #ffffff;
+`;
+
+const WeatherTable = (props) => {
+  const { data } = props;
+  return (
+    <div>
+      {data ? (
+        <WeatherCard>
+          <CityName>{data.city.name}</CityName>
+
+          <DescriptionIcon
+            src={`http://openweathermap.org/img/w/${data?.list[0].weather[0].icon}.png`}
+            alt="weather icon"
+          />
+          <WeatherInfo>{data.list[0].weather[0].description}</WeatherInfo>
+
+          <WeatherInfo>
+            Temperature: {data.list[0].main.temp} degrees Fahrenheit
+          </WeatherInfo>
+          <WeatherInfo>
+            Feels like: {data.list[0].main.feels_like} degrees Fahrenheit
+          </WeatherInfo>
+          <WeatherInfo>Wind: {data.list[0].wind.speed} mph</WeatherInfo>
+        </WeatherCard>
+      ) : (
+        <SearchInstructions>
+          Search your city for weather info
+        </SearchInstructions>
+      )}
+    </div>
+  );
+};
+
+export default WeatherTable;
